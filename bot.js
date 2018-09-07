c48602b484176ce40ea32a57f848a7b42f9f0e07
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-09-07 20:08:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
 import discord
 import sqlib
 import re
